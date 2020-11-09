618ffae0b31c2080b5dc400acd1f4130881c01b3
jeff122iq <jeff122iq@gmail.com>
2020-11-09 23:22:22 +0200

    Create basic components

---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Container = () => {
+    return(
+        <div className="container">
+            <div className="logo"></div>
+        </div>
+    )
+}
+
+export default Container
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Container from "../Container/Container"
+
+const Header = () => {
+    return(
+        <header className="pageHeader">
+            <Container>
+            </Container>
+        </header>
+    )
+}
+
+export default Header
